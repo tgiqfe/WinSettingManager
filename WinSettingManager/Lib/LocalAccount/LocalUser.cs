1e954ef6b9f558de503c5391ad1626dc42907bfb
tqji31001 <tqji31001@gmail.com>
2025-10-17 00:35:43 +0900

    check update code.

--- WinSettingManager/Lib/LocalAccount/LocalUser.cs
+++ WinSettingManager/Lib/LocalAccount/LocalUser.cs
@@ -10,15 +10,16 @@ namespace WinSettingManager.Lib.LocalAccount
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public bool PasswordMustChange { get; set; }
-        public bool PasswordCannotChange { get; set; }
+        public bool UserMustChangePasswordAtNextLogon { get; set; }
+        public bool UserCannotChangePassword { get; set; }
         public bool PasswordNeverExpires { get; set; }
-        public bool Enabled { get; set; }
-        public bool AccountLocked { get; set; }
+        public bool AccountIsDisabled { get; set; }
+        public bool AccountIsLockedOut { get; set; }
         public string[] JoinedGroup { get; set; }
         public string ProfilePath { get; set; }
         public string LogonScript { get; set; }
         public string HomeDirectory { get; set; }
         public string HomeDrive { get; set; }
+        public string SID { get; set; }
     }
 }
---
+++ WinSettingManager/Lib/LocalAccount/LocalUserFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinSettingManager.Lib.LocalAccount
+{
+    internal class LocalUserFunctions
+    {
+        public static LocalUser[] GetLocalUsers()
+        {
+            List<LocalUser> localUsers = new();
+
+            
+            var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_UserAccount WHERE LocalAccount=True");
+            var results = searcher.Get();
+            foreach (var result in results)
+            {
+                LocalUser user = new LocalUser
+                {
+                    Name = result["Name"]?.ToString(),
+                    Description = result["Description"]?.ToString(),
+                    AccountIsDisabled = (bool)(result["Disabled"] ?? false),
+                    AccountIsLockedOut = (bool)(result["Lockout"] ?? false),
+                };
+                localUsers.Add(user);
+
+                
+            }
+
+            
+
+            return localUsers.ToArray();
+        }
+    }
+}
